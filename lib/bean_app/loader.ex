c0a9742329e6d086c4827370f6e37848f687a87c
Lorenzo Lopez Sancho <lorenzo.lopez@intec.es>
2019-07-28 09:37:13 +0200

    Add Data and Loader module

---
+++ lib/bean_app/loader.ex
@@ -0,0 +1,20 @@
+defmodule BeanApp.Loader do
+  alias BeanApp.Merchant
+
+  def load_merchants do
+    File.stream!("data/merchants.txt")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&Merchant.write/1)
+  end
+
+  def load_transactions do
+    File.stream!("data/transactions.txt")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&queue_transaction/1)
+  end
+
+  def queue_transaction(description) do
+    {:classify, [description]} |> Honeydew.async(:transactions)
+  end
+end
+
